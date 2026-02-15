aaf44935cddc09231194f419f8afd647a35f2417
AnilaAtlas <anila.khan3044@gmail.com>
2026-02-15 22:00:36 +0500

    redeployed

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 import dns from "node:dns/promises";
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
-const port = process.env.PORT || 4002;
+const port = process.env.PORT || 4001;
+
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -25,7 +26,15 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.json({ message: 'EchoNote API is running!' });
+});
+
+app.get('/api/v1/noteapp', (req, res) => {
+    res.json({ message: 'NoteApp API endpoints are working!' });
+});
 app.use("/api/v1/noteapp", note_routes);
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
 });
+
